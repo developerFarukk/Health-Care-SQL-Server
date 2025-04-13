512c4d89174476d655993825026dcfe07ea33b9b
FARUK2059 <omarfaruk34552@gmail.com>
2025-04-14 01:33:12 +0600

    upsate admin route

--- src/app/modules/Admin/admin.routes.ts
+++ src/app/modules/Admin/admin.routes.ts
@@ -3,6 +3,8 @@ import express, { NextFunction, Request, Response } from 'express';
 import { AdminController } from './admin.controller';
 import validateRequest from '../../middlewares/validateRequest';
 import { adminValidationSchemas } from './admin.validations';
+import auth from '../../middlewares/auth';
+import { UserRole } from '@prisma/client';
 
 const router = express.Router();
 
@@ -10,7 +12,7 @@ const router = express.Router();
 // get all user
 router.get(
     '/',
-    // auth(UserRole.SUPER_ADMIN, UserRole.ADMIN),
+    auth(UserRole.SUPER_ADMIN, UserRole.ADMIN),
     AdminController.getAllAdmin
 );
 
@@ -18,14 +20,14 @@ router.get(
 // get single data by ID
 router.get(
     '/:id',
-    // auth(UserRole.SUPER_ADMIN, UserRole.ADMIN),
+    auth(UserRole.SUPER_ADMIN, UserRole.ADMIN),
     AdminController.getById
 );
 
 // Update admin route
 router.patch(
     '/:id',
-    // auth(UserRole.SUPER_ADMIN, UserRole.ADMIN),
+    auth(UserRole.SUPER_ADMIN, UserRole.ADMIN),
     validateRequest(adminValidationSchemas.update),
     AdminController.updateAdmin
 );
@@ -34,7 +36,7 @@ router.patch(
 // delete admin route
 router.delete(
     '/:id',
-    // auth(UserRole.SUPER_ADMIN, UserRole.ADMIN),
+    auth(UserRole.SUPER_ADMIN, UserRole.ADMIN),
     AdminController.deleteAdmin
 );
 
@@ -42,7 +44,7 @@ router.delete(
 // soft delete route
 router.delete(
     '/soft/:id',
-    // auth(UserRole.SUPER_ADMIN, UserRole.ADMIN),
+    auth(UserRole.SUPER_ADMIN, UserRole.ADMIN),
     AdminController.softDelete
 );
 
