ddf43dd409f862efd428e6bee1fc52e42f417880
FARUK2059 <omarfaruk34552@gmail.com>
2025-04-15 21:33:28 +0600

    add i s

---
+++ src/app/modules/Schedule/schedule.interface.ts
@@ -0,0 +1,12 @@
+
+export type ISchedule = {
+    startDate: string;
+    endDate: string;
+    startTime: string;
+    endTime: string;
+}
+
+export type IFilterRequest = {
+    startDate?: string | undefined;
+    endDate?: string | undefined;
+}
